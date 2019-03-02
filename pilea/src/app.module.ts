faad9c2d671f48ac97240ef091adf8c5e81287ad
Fabien Le Frapper <contact@fabienlefrapper.me>
2019-03-02 15:43:54 +0100

    Setup serverless

--- pilea/src/app.module.ts
+++ pilea/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlantsController } from './plants/plants.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PlantsController],
   providers: [AppService],
 })
 export class AppModule {}
